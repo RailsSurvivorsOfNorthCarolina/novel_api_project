9d41b340fee128cb7d0dde24aa9ea7446ca7e0b3
Aaron Wiggins <apw7177@gmail.com>
2015-05-28 17:03:09 -0400

    require minitest/pride

--- app/models/game_review.rb
+++ app/models/game_review.rb
@@ -1,2 +1,9 @@
 class GameReview
+  def initialize(game)
+    @page = get_data
+  end
+
+  private def get_data
+    get_data = HTTParty.get("https://videogamesrating.p.mashape.com/get.php?count=5&game=#{game}", headers: {"X-Mashape-Key" => "#{ENV["IGN_KEY"]}","Accept" => "application/json"})
+  end
 end
--- test/models/game_review_test.rb
+++ test/models/game_review_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
+class GameReviews
+  private def get_data
+    @page = Jason.parse(File.open("game_review.json").read)
+  end
+end
+
 class GameReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
 end
