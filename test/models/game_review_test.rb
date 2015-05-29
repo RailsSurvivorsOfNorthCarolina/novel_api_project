55decadd30321a8d9dd37352d7cbe2eebdc621d3
Danai Adkisson <dadkisso@live.com>
2015-05-29 09:53:39 -0400

    ignore

--- test/models/game_review_test.rb
+++ test/models/game_review_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
-class GameReviews
-  private def get_data
-    @page = Jason.parse(File.open("game_review.json").read)
-  end
-end
+# class GameReviews
+#   private def get_data
+#     @page = Jason.parse(File.open("game_review.json").read)
+#   end
+# end
 
 class GameReviewTest < ActiveSupport::TestCase
   test "the truth" do
