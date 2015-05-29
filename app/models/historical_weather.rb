5dd3f9eb5afa7ab8ee5260dd56ba6cd4c25c9a37
Danai Adkisson <dadkisso@live.com>
2015-05-29 13:08:52 -0400

    ignore

--- app/models/historical_weather.rb
+++ app/models/historical_weather.rb
@@ -1,2 +1,14 @@
 class HistoricalWeather
+  def initialize
+    @weather_data = get_data
+    @date = date
+  end
+
+  def temperature
+
+  end
+
+  def get_data
+    HTTParty.get("http://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/history_#{@date}/q/CA/San_Francisco.json")
+  end
 end
--- test/models/historical_weather_test.rb
+++ test/models/historical_weather_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
+require 'historical_weather'
 
 class HistoricalWeatherTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+
+  def test_can_get_temperature
+    temp = HistoricalWeather.new(20060405)
+    assert_equal 50, temp.temperature
   end
+
 end
