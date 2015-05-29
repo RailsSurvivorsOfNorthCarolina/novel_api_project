cb9fe83d82783e2c784452f240fca41528e92926
Danai Adkisson <dadkisso@live.com>
2015-05-29 13:41:57 -0400

    weather api readly to roll???

--- test/models/historical_weather_test.rb
+++ test/models/historical_weather_test.rb
@@ -11,8 +11,20 @@ class HistoricalWeatherTest < ActiveSupport::TestCase
 
 
   def test_can_get_temperature
-    temp = HistoricalWeather.new(20060405)
-    assert_equal "50.0", temp.temperature
+    weather_date = HistoricalWeather.new(20060405)
+    assert_equal "50.0", weather_date.temperature
+  end
+
+
+  def test_can_get_inches_of_rain
+    weather_date = HistoricalWeather.new(20060405)
+    assert_equal "0", weather_date.rainfall
+  end
+
+
+  def test_can_get_climate_conditions
+    weather_date = HistoricalWeather.new(20060405)
+    assert_equal "Mostly Cloudy", weather_date.climate_conditions
   end
 
 end
