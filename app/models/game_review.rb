5601edb81f8d6d01a886739b0fa421a8c4c0eccd
Aaron Wiggins <apw7177@gmail.com>
2015-05-29 09:52:20 -0400

    test game_review_model. index controller for game review

--- app/models/game_review.rb
+++ app/models/game_review.rb
@@ -1,9 +1,24 @@
 class GameReview
   def initialize(game)
-    @page = get_data
+    @game = game
+    @page = get_data.first
+    #HTTParty.get("https://videogamesrating.p.mashape.com/get.php?count=5&game=#{game}",
+                          # headers: {"X-Mashape-Key" => "#{ENV['IGN_KEY']}","Accept" => "application/json"})
   end
 
-  private def get_data
-    get_data = HTTParty.get("https://videogamesrating.p.mashape.com/get.php?count=5&game=#{game}", headers: {"X-Mashape-Key" => "#{ENV["IGN_KEY"]}","Accept" => "application/json"})
+  def review_score
+    @page["score"].to_f
+  end
+
+  def get_title
+    @page["title"]
+  end
+
+  def review_date
+    @page["short_description"].match(/\A[A-Z]\w+\s\d{2}\,\s\d{4}\z/)
+  end
+
+  def get_data
+    HTTParty.get("https://videogamesrating.p.mashape.com/get.php?count=5&game=#{@game}", headers: {"X-Mashape-Key" => "#{ENV["IGN_KEY"]}","Accept" => "application/json"})
   end
 end
